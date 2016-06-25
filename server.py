5928be38a165c6c448f126eb96026e4325258c28
oodeagleoo <oodeagleoo@live.de>
2016-06-25 19:04:29 +0200

    added println

--- server.py
+++ server.py
@@ -13,21 +13,27 @@ def createServer(host, port):
     s.bind((host, port))
     s.listen(1)
 
+    print('Listening to port ' + port)
+
     while True:
 
         c, address = s.accept()
         print('Connected to ', address)
 
         command = socketHelper.recvData(c).decode()
+        print('Command received: ' + command)
 
         if command == 'capture':
 
             fileName = 'image ' + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + '.jpg'
             raspEYE.takePicture(fileName)
+            print('Picture taken')
 
+            print('Start sending picture...')
             socketHelper.sendData(c, fileName.encode())
             socketHelper.sendFile(c, fileName)
             socketHelper.sendData(c, 'Finished'.encode())
+            print('Finished sending')
 
 
 if __name__ == '__main__':
