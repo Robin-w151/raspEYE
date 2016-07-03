7946656872db0286895b81c2bc038c0d6237835d
oodeagleoo <oodeagleoo@live.de>
2016-07-03 15:18:59 +0200

    bug fix

--- server.py
+++ server.py
@@ -68,7 +68,7 @@ class Server(threading.Thread):
 
 if __name__ == '__main__':
 
-    server = Server('192.168.1.15', 12345)
+    server = Server('192.168.1.150', 12345)
     server.start()
 
     while True:
