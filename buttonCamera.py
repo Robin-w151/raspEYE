892e7b71d990b69d9c8e2cf3df62b259ec1acc8e
oodeagleoo <oodeagleoo@live.de>
2016-07-19 22:35:28 +0200

    added button

---
+++ buttonCamera.py
@@ -0,0 +1,20 @@
+import raspEYE
+import time
+import datetime
+
+from gpiozero import Button
+
+
+def main():
+
+    while True:
+
+        button = Button(4)
+        button.wait_for_press()
+
+        fileName = 'image ' + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + '.png'
+
+        raspEYE.takePicture(filename=fileName, sec=0, res=(2592, 1944))
+
+
+if __name__ == '__main__':
